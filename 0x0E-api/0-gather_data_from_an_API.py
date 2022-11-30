55c0b6fbeddb14f70d98a57b0471d6e995aac099
conkobar <connorkleine@gmail.com>
2022-11-30 17:26:40 -0600

    trying this for reqs

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 """returns info about employee's todo list"""
-import requests
 from sys import argv
+import requests
 
 
 if __name__ == "__main__":
+
+    # get the given employees info
     employee = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     ).json()
 
+    # get the employees todo list
     todos = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1])
     ).json()
 
+    # make it an actual list object
     todo_list = [
         task.get("title") for task in todos if task.get("completed") is True
     ]
+
+    # format and print response
     print(
         "Employee {} is done with tasks({}/{}):".format(
             employee.get("name"), len(todo_list), len(todos)
