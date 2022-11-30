1a73ec7850eddb88a7999eb397d3888308e94f2d
conkobar <connorkleine@gmail.com>
2022-11-30 17:29:51 -0600

    my code is uglier i hope the checker is happy

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -8,15 +8,12 @@ if __name__ == "__main__":
     employee = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     ).json()
-
     todos = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1])
     ).json()
-
     todo_list = [
         task.get("title") for task in todos if task.get("completed") is True
     ]
-
     print(
         "Employee {} is done with tasks({}/{}):".format(
             employee.get("name"), len(todo_list), len(todos)
