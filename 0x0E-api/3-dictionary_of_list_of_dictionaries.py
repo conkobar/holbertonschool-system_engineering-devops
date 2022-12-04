9130d28c60c90fa5f994b86523d4d1a4510d4c95
conkobar <connorkleine@gmail.com>
2022-12-04 16:08:30 -0600

    task 3 but it is not working right at all haha

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""exports data in json format"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    # get the given employees info
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users/"
+    ).json()
+
+    # get the employees todo list
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/"
+    ).json()
+
+    # make it an actual list object
+    user_info = {
+        user['id']: user['username'] for user in users
+    }
+    tasks = {
+        task['userId']: {
+            'username': user_info[task['userId']],
+            'task': task['title'],
+            'completed': task['completed']
+        } for task in todos
+    }
+
+    # format and print response
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(tasks, f)
